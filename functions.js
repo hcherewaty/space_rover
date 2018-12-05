1228002e359928d2f91c1c6e43d2fd04ea9b5da1
MichaelJahns <jahnsmic001@gmail.com>
2018-12-05 12:55:59 -0800

    saving before pull

--- functions.js
+++ functions.js
@@ -1,12 +1,25 @@
-function calculateDistance(){
-    http://www.astro-phys.com/api/de406/states?date=${inputed-date}&bodies=${inputed-destination}
-    http://www.astro-phys.com/api/de406/states?date=${inputed-date}&bodies=earth
+http://www.astro-phys.com/api/de406/states?date=${inputed-date}&bodies=${inputed-destination}
+http://www.astro-phys.com/api/de406/states?date=${inputed-date}&bodies=earth
 
-    const difference(destination, startPoint, unit){
+function Triangulate(location){
+    // this.Xposition =
+    // this.Yposition = 
+    // this.Zposition =
+}
+function getStartPoint(request, response){
+    let url = `http://www.astro-phys.com/api/de406/states?date=${inputed-date}&bodies=earth`
+
+    return superagent.get(url)
+        .then(location => {
+            const startPoint = new Triangulate(location);
+            .then(startPoint => response.send(startPoint))
+        })
+        // .catch(error => handleError(error));
+
+}
+function calculateDistance(destination, startPoint, unit){
         const totalDistance = 0;
         if(unit === 'km'){
-            totalDistance = SQRT(((destination.X − startPoint.X)^2 + (destination.Y − startPoint.Y)^2 + (destination.Y− startPoint.Z)^2))
+            totalDistance = SQRT(((destination.X - startPoint.X)^2 + (destination.Y - startPoint.Y)^2 + (destination.Y - startPoint.Z)^2))
         }
-
-    } 
 }
