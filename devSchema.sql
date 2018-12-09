1fadea3a62d5f8bb3900f4b43cde007205cffe6d
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-12-09 10:51:42 -0800

    Updates schema file with more team member info

--- devSchema.sql
+++ devSchema.sql
@@ -15,3 +15,12 @@ CREATE TABLE devs (
 
 INSERT INTO devs (name, image_url, bio, email, github, twitter, linkedin)
 VALUES('Michael Jahns', 'https://photos.app.goo.gl/AeqhQG8EHX8f9ygc8', 'The thing about Michael is, he constantly breaks into the unknown, bursting ever further and higher into the infinite abyss of the greater cosmos.', 'Jahnsmic001@gmail.com', 'https://github.com/MichaelJahns', '#','https://www.linkedin.com/in/michaelcjahns/' );
+
+INSERT INTO devs (name, bio, email, github, twitter, linkedin)
+VALUES('Becca Lee', 'Becca is a software developer with a background in marketing and customer service, who fell in love with the tech industry while working as the marketing manager for a JavaScript consultancy. She is a voracious learner who loves to translate her knowledge and intuition into thoughtful, user-focused strategies. She is currently studying JavaScript development at Code Fellows and is excited to use her new skills to create user-centric apps and solve challenging software problems. Her favorite planet is Saturn because those rings really make it something special.', 'rebecca.kirstin.lee@gmail.com', 'https://github.com/beccalee123', 'https://twitter.com/the_becca_lee', 'https://www.linkedin.com/in/becca-lee-805922110/');
+
+INSERT INTO devs (name, bio, email, github, twitter, linkedin)
+VALUES('Siobhan Niess', 'Siobhan is a mother to an amazing little girl who helps her push herself to be everything she can be and more. She has lived in Washington all her life and absolutely loves the Pacific Northwest. In her spare time, Siobhan enjoys being outside, whether she is hiking up to water falls or just taking her daughter to the park, anything to just get out of the house. She feels that spending time with your family is very important, we help each other become stronger and make it through the tough moments in life. When it comes to planets, her favorite one is Mercury by far because it is simply breathtaking to look at.', 'niesssiobhan@live.com', 'https://github.com/niesssiobhan', '#', 'https://www.linkedin.com/in/siobhan-niess-45351ba7/');
+
+INSERT INTO devs (name, bio, email, github, twitter, linkedin)
+VALUES('Heather Cherewaty', 'Bio for Heather', 'hr.cherewaty@gmail.com', 'https://github.com/hcherewaty', '#', 'https://www.linkedin.com/in/hcherewaty/');
